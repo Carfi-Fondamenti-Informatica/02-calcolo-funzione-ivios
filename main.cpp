bb4c4416eef8fcc07947723b0c2bfc2dbf69f6f0
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-11-14 18:10:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main() {
 
     float z=0;
 
-    if ((x<0)||(y>0)) {
+    if ((x<0)&&(y>0)) {
         z = (a*x)-(b*y);
-    } else if ((x>=0)||(y<=0)) {
+    } else if ((x>=0)&&(y<=0)) {
         z = (a*x*x)-(b*y);
     } else {
         z = (a*x) + (b*y*y);
